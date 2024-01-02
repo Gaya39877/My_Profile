5162775267a2beb5ae4ebf7acb90a5a4d0330b15
Gayathri <rasangikahw@gmail.com>
2024-01-02 17:56:36 +0530

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project01.png";
-import projImg2 from "../assets/img/project-img2.png";/*Image must change*/
-import projImg3 from "../assets/img/project-img3.png";/*Image must change*/
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
